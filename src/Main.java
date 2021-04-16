1feaf98f6ef491419da5806485545ea8018d0f4d
G-Delgado <gabriel.delgado1@u.icesi.edu.co>
2021-04-16 16:59:17 -0500

    I believe the algorithm is alright but there is an error somewhere

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,29 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.Stack;
 
 
 public class Main {
 	
+	private Stack<Integer> firstRod;
+	private Stack<Integer> midRod;
+	private Stack<Integer> endRod;
+	private PrintWriter pw;
+	
 	public Main() {
-		
+		firstRod = new Stack<>();
+		midRod = new Stack<>();
+		endRod = new Stack<>();
 	}
 
 	public static void main(String[] args) {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		//BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		PrintWriter pw;
 		Main ppal = new Main();
 		
 		try {
-			pw = new PrintWriter("./data/result.txt");
+			ppal.pw = new PrintWriter("./data/result.txt");
 			int cases = Integer.parseInt(br.readLine());
 			String line = "";
 			int i = 0;
@@ -26,12 +33,12 @@ public class Main {
 				if (line != null) {
 					String result = ppal.solveHanoiStart(line);
 					//System.out.println(result);
-					pw.println(result + "\n");
+					ppal.pw.println(result + "\n");
 				}
 				i++;
 			}
 			br.close();
-			pw.close();
+			ppal.pw.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -39,28 +46,35 @@ public class Main {
 	
 	public String solveHanoiStart(String discs) {
 		int n = Integer.parseInt(discs);
-		String result = solveHanoi(n, n, 0, 0);
+		//pw.println(discs + " 0 " + "0");
+		String result = discs + " 0 " + "0\n";
+		while (firstRod.size() < n) {
+			firstRod.add(firstRod.size() + 1);
+		}
+		result += solveHanoi(n, firstRod, midRod, endRod);
 		//System.out.println(result + "\nSAPE!");
+		firstRod.clear();
+		midRod.clear();
+		endRod.clear();
 		return result;
 	}
 	
 
 
-	public String solveHanoi(int n, int firstRod, int secondRod, int thirdRod) {
+	public String solveHanoi(int n, Stack<Integer> first, Stack<Integer> second, Stack<Integer> third) {
 		String result = "";
 		if (n == 1) {
-			//System.out.println(firstRod + " - " + thirdRod);
-			result += firstRod + " " + secondRod + " " + thirdRod + "\n";
-			firstRod -= 1;
-			thirdRod += 1;
-			result += firstRod + " " + secondRod + " " + thirdRod + "\n";
+			third.add(first.pop());
+			// Se cambian los nombres del print, puesto que de lo contrario, el orden estaría intercambiado.
+			//pw.println(firstRod.size() + " " + midRod.size() + " " + endRod.size());
+			result += firstRod.size() + " " + midRod.size() + " " + endRod.size()+ "\n";
 			
-			//System.out.println(firstRod + " " + secondRod + " " + thirdRod);
 		} else {
-			result += solveHanoi(n - 1, firstRod, secondRod, thirdRod);
-			//System.out.println(firstRod + " - " + thirdRod);
-			result += solveHanoi(n - 1, secondRod, thirdRod, firstRod);
-			//System.out.println(firstRod + " - " + thirdRod);
+			solveHanoi(n - 1, first, third, second);
+			third.add(first.pop());
+			//pw.println(firstRod.size() + " " + midRod.size() + " " + endRod.size());
+			result += firstRod.size() + " " + midRod.size() + " " + endRod.size()+ "\n";
+			solveHanoi(n - 1, second, first, third);
 		}
 		return result;
 		
